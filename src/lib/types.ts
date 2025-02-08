d3ba1afd9162bb143f4fb10b9109cf60f119a2c6
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2025-02-08 08:52:55 +0600

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,9 @@
 export interface Note {
   id: string
   content: string
-  isPublic: boolean
-  expiresIn: string
   createdAt: number
   updatedAt: number
-}
-
-export interface ApiResponse<T> {
-  success: boolean
-  data?: T
-  error?: string
+  isPublic: boolean
+  expiresIn?: number
 }
 
